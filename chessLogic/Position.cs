2618713dcb87af491b5fa2caf9bb1861b831f1f6
William Wassmann <wmwassmann@hotmail.com>
2024-05-29 14:12:00 -0700

    Add project files.

---
+++ chessLogic/Position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column) 
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Player SquareColor()
+        {
+            if ((Row + Column) % 2 == 0)
+            {
+                return Player.White;               
+            }
+
+            return Player.Black;
+        }
+}
