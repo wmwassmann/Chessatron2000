ba2be154cdc822f13f9ff26e15048fdf5f2babf3
William Wassmann <wmwassmann@hotmail.com>
2024-07-18 12:09:55 -0700

    Added in more legal move checks and endgame methods. Queen, rook, and bishop are a bit bugged. Unsure why the black queen can't put the opposing king into check.

---
+++ chessLogic/EndReason.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ChessLogic
+{
+    public enum EndReason
+    {
+        Checkmate,
+        Stalemate,
+        FiftyMoveRule,
+        InsufficientMaterial,
+        ThreefoldRepetition
+    }
+}
---
+++ chessLogic/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace ChessLogic
+{
+    public class Result
+    {
+        public Player Winner { get; }
+
+        public EndReason Reason { get; }
+
+        public Result(Player winner, EndReason reason)
+        {
+            Winner = winner;
+            Reason = reason;
+        }
+
+        public static Result Win(Player winner)
+        {
+            return new Result(winner, EndReason.Checkmate);
+        }
+
+        public static Result Draw(EndReason reason) 
+        {
+            return new Result(Player.none, reason);
+        }
+
+    }
+}
