fd43bc6b6a9c227c1244a6e77d11ac31bb5f7e22
William Wassmann <wmwassmann@hotmail.com>
2024-07-24 14:15:50 -0700

    More additions to menus and game states

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -62,6 +62,13 @@ namespace ChessUI
 
         private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+
+            if (IsMenuOnScreen())
+            {
+                return;
+            }
+
+
             Point point = e.GetPosition(BoardGrid);
 
             Position pos = ToSquarePosition(point);
@@ -102,6 +109,11 @@ namespace ChessUI
             gameState.MakeMove(move);
             DrawBoard(gameState.Board);
             SetCursor(gameState.CurrentPlayer);
+
+            if (gameState.IsGameOver())
+            {
+                ShowGameOver();
+            }
         }
 
         private void OnToPositionSelected(Position pos)
@@ -152,6 +164,38 @@ namespace ChessUI
             {
                 Cursor = ChessCursors.BlackCursor;
             }
+        }
+
+        private bool IsMenuOnScreen()
+        {
+            return MenuContainer.Content != null;
+        }
+
+        private void ShowGameOver()
+        {
+            GameOverMenu gameOverMenu = new GameOverMenu(gameState);
+            MenuContainer.Content = gameOverMenu;
+            gameOverMenu.OptionSelected += option =>
+            {
+                if (option == Option.Restart)
+                {
+                    MenuContainer.Content = null;
+                    RestartGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            };
+        }
+
+        private void RestartGame()
+        {
+            HideHighlights();
+            moveCache.Clear();
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
         }
 
     }
